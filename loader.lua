cd7f2348595e3bdb32bdf0a6962823304a3e7440
rysscripts <ryan.dhudwarr@gmail.com>
2026-04-29 15:47:13 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjA4OTUxMzE2LTlkMDktNGQxNC04OWIzLTc4YzgwYmZiMzVkMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJuZXd1aSJ9fQ"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IiIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJuZXd1aSJ9fQ"))()
